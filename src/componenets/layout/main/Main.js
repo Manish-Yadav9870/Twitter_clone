c9fa60a7cd2d8f1d04b71cb3625d9cf0d07e3612
Manish Yadav <myadav9870@gmail.com>
2024-09-27 17:50:05 +0530

    update

--- src/componenets/layout/main/Main.js
+++ src/componenets/layout/main/Main.js
@@ -1,13 +1,19 @@
 import './Main.scss'
 import MainFirstCont from './Main_First_Cont/Main_First_Cont';
+import MainFourthCont from './Main_Fourth_Cont/Main_Fourth_Cont';
+import MainSecondCont from './Main_Second_Cont/Main_Second_Cont';
+import MainThirdCont from './Main_Third_Cont/Main_Third_Cont';
 
 const MainContant=()=>{
     return(
         <main className="mainbar">
 <div className="Main_First_Cont">
     <MainFirstCont/>
+    <MainSecondCont/>
+    <MainThirdCont/>
+    <MainFourthCont/>
 </div>
-<div  className="Main_Second_Cont">
+<div  className="Main_Second_Cont_">
     Main_Second_Cont
 </div>
         </main>
---
+++ src/componenets/layout/main/Main_Third_Cont/Main_Third_Cont.js
@@ -0,0 +1,13 @@
+import './Main_Third_Cont.scss'
+
+const MainThirdCont=()=>{
+    return(
+        <div className="Main_Third_Cont">
+<div className="Main_Third_Child_Cont">
+Show 335 posts
+</div>
+
+        </div>
+    )
+}
+export default MainThirdCont;
